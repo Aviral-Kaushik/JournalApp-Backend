704b3e734e835c0afe8af8c3a158bfc6d4653d4f
Aviral Kaushik <zaviralkaushik@gmail.com>
2025-09-02 13:42:28 +0530

    Implemented JWT Token based Authentication & Authorization instead of Basic Auth in all secured endpoints and also added JWT Based functionality for /signup and /login endpoints.

--- src/main/java/com/aviral/journalApp/config/SpringSecurity.java
+++ src/main/java/com/aviral/journalApp/config/SpringSecurity.java
@@ -1,17 +1,20 @@
 package com.aviral.journalApp.config;
 
+import com.aviral.journalApp.filter.JWTFilter;
 import com.aviral.journalApp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,14 +23,17 @@ public class SpringSecurity {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
+    @Autowired
+    private JWTFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(request -> request
-                .requestMatchers("/health", "/create-user").permitAll()
+                .requestMatchers("/health", "/signup", "/login").permitAll()
                 .requestMatchers("/journal/**", "/user/**").authenticated()
-                .requestMatchers("/admin/**", "/user/**").hasRole("ADMIN")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
@@ -40,5 +46,10 @@ public class SpringSecurity {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 }
--- src/main/java/com/aviral/journalApp/controller/PublicController.java
+++ src/main/java/com/aviral/journalApp/controller/PublicController.java
@@ -1,27 +1,62 @@
 package com.aviral.journalApp.controller;
 
 import com.aviral.journalApp.entity.User;
+import com.aviral.journalApp.service.UserDetailsServiceImpl;
 import com.aviral.journalApp.service.UserService;
+import com.aviral.journalApp.utils.JWTUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PublicController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @GetMapping("health")
     public String healthCheck() {
         return "Ok!";
     }
 
-    @PostMapping("create-user")
-    private User createUser(@RequestBody User user) {
+    @PostMapping("signup")
+    private User signup(@RequestBody User user) {
         userService.createUser(user);
         return user;
+    }
+
+    @PostMapping("login")
+    private ResponseEntity<?> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    user.getUserName(), user.getPassword()));
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+
+            String jwtToken = jwtUtil.generateToken(userDetails.getUsername());
+
+            return new ResponseEntity<>(jwtToken, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception while authenticating or in login functionality: {}", e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
     }
 }
